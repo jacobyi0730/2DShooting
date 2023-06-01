b56a255880ecbe8a7824ceaf64ecf0ebbc12e9f7
jacobyi0730 <jacobyi0730@gmail.com>
2023-06-01 17:17:17 +0900

    Initial commit

---
+++ Assets/PlayerFire.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 사용자가 마우스 왼쪽 버튼을 누르면 
+// 총알공장에서 총알을 만들어서
+// 총구위치에 배치하고싶다.
+public class PlayerFire : MonoBehaviour
+{
+    public GameObject bulletFactory;
+    public Transform firePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 만약 사용자가 마우스 왼쪽 버튼을 누르면 
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // 총알공장에서 총알을 만들어서
+            GameObject bullet = Instantiate(bulletFactory);
+            // 총구위치에 배치하고싶다.
+            bullet.transform.position = firePosition.position;
+            bullet.transform.up = firePosition.up;
+        }
+
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 사용자의 입력에따라 상하좌우로 이동하고싶다.
+// 필요요소
+// - 속력
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1. 사용자의 입력에따라
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        print(h + ", " + v);
+        // 2. 상하좌우로 방향을 만들고
+        Vector3 dir = h * Vector3.right + v * Vector3.up;
+        // dir의 크기를 1로 만들고싶다.
+        dir.Normalize();
+        // 3. 그 방향으로 이동하고싶다. P = P + vt
+        Vector3 velocity = dir * speed;
+        transform.position += velocity * Time.deltaTime;
+
+    }
+}
